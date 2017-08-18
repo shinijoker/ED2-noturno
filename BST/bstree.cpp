e35b4a1cfb9ace469ea2bab3a7f70b07ffdb39e2
aluno-uffs <ricardo.dparizotto@gmail.com>
2017-08-17 22:14:08 -0300

    bst insert, search

--- BST/bstree.cpp
+++ BST/bstree.cpp
@@ -13,7 +13,14 @@ struct node{
 //busca por uma chave k na árvore com raiz r
 //caso a chave não estiver na árvore retorna NULL
 
-node * search (node * r, int key);
+node * search (node * r, int key){
+	if(!r || r->key == key ) return r;				//n encontrou a chave
+
+	if(key <= r->key) return search (r->left, key);
+	
+	return search (r->right, key); 	
+}
+
 
 //mostra todas as chaves da árvore
 
@@ -22,7 +29,33 @@ void * print (node * r);
 //insere um nodo com chave key na árvore com raiz r
 //retorna um ponteiro para a raiz da árvore
 
-node * insert (node * r, int key);
+node * insert (node * r, int key){
+	node * n = (node *)malloc(sizeof(node));		//cria um novo nodo
+	n->left = n->right = NULL;
+	n->key = key;
+			
+	if(!r) return n;			// se a arvore estiver vazia
+
+	node * it = r;	
+
+	while(1){
+		if(key < it->key){
+			if(!it->left){
+				it->left = n; 		//atualiza o filho da esquerda de r	
+				break;		
+			}
+			it = it->left;	
+		}else{
+			if(!it->right){
+				it->right = n;
+				break;			
+			}
+			it = it->right;
+		}		
+	}
+
+	return r;
+}
 
 //remove o nodo da árvore com raiz r que possui chave igual a key
 //(lembra que tem que dar free no nodo)
@@ -31,6 +64,22 @@ void remove (node * r, int key);
 
 
 int main ( void ){
+
+	int n;
+	
+	node * root = NULL;
+
+	
+	while(scanf("%d", &n)){
+		root = insert(root, n);
+	}
+
+	if(search(root, 7)) printf("achouUU!");
+	else printf("nao achooou :(");
+	
+	
+	
+
 	return 0;
 }
 
